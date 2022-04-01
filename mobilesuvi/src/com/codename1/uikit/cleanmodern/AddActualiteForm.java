eb7e443e1b66c090a7847e74100ab0ce7b10e304
sahraouirayen <60189198+sahraouirayen@users.noreply.github.com>
2022-04-01 01:23:32 +0100

    commit mobile

---
+++ mobilesuvi/src/com/codename1/uikit/cleanmodern/AddActualiteForm.java
@@ -0,0 +1,170 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.codename1.uikit.cleanmodern;
+
+import com.codename1.components.MultiButton;
+import com.codename1.components.ScaleImageLabel;
+import com.codename1.ui.Button;
+import com.codename1.ui.ButtonGroup;
+import com.codename1.ui.ComboBox;
+import com.codename1.ui.Command;
+import com.codename1.ui.Component;
+import static com.codename1.ui.Component.BOTTOM;
+import static com.codename1.ui.Component.CENTER;
+import com.codename1.ui.Container;
+import com.codename1.ui.Dialog;
+import com.codename1.ui.Display;
+import com.codename1.ui.FontImage;
+import com.codename1.ui.Form;
+import com.codename1.ui.Graphics;
+import com.codename1.ui.Image;
+import com.codename1.ui.Label;
+import com.codename1.ui.RadioButton;
+import com.codename1.ui.Tabs;
+import com.codename1.ui.TextField;
+import com.codename1.ui.Toolbar;
+import com.codename1.ui.events.ActionEvent;
+import com.codename1.ui.events.ActionListener;
+import com.codename1.ui.layouts.BorderLayout;
+import com.codename1.ui.layouts.BoxLayout;
+import com.codename1.ui.layouts.FlowLayout;
+import com.codename1.ui.layouts.LayeredLayout;
+import com.codename1.ui.list.GenericListCellRenderer;
+import com.codename1.ui.plaf.Style;
+import com.codename1.ui.util.Resources;
+import com.codename1.myapp.entities.Actualite;
+import com.codename1.myapp.Services.ServiceActualite;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+/**
+ *
+ * @author sahra
+ */
+public class AddActualiteForm extends BaseForm {
+           ArrayList<Actualite> list;
+ 
+              
+    private Map<String, Object> createListEntry(String name, String date) {
+    Map<String, Object> entry = new HashMap<>();
+    entry.put("Line1", name);
+    entry.put("Line2", date);
+    return entry;
+}
+ 
+    public AddActualiteForm(Form previous,Resources res) {
+         super("Artwork", BoxLayout.y());
+        Toolbar tb = new Toolbar(true);
+        setToolbar(tb);
+        getTitleArea().setUIID("Container");
+        setTitle("Add Artwork");
+        getContentPane().setScrollVisible(false);
+        
+        super.addSideMenu(res);
+        //tb.addSearchCommand(e -> {});
+                Tabs swipe = new Tabs();
+                        Label spacer1 = new Label();
+        Label spacer2 = new Label();
+
+        addTab(swipe,  spacer1);
+          swipe.setUIID("Container");
+        swipe.getContentPane().setUIID("Container");
+        swipe.hideTabs();
+        ButtonGroup bg = new ButtonGroup();
+        int size = Display.getInstance().convertToPixels(1);
+        Image unselectedWalkthru = Image.createImage(size, size, 0);
+        Graphics g = unselectedWalkthru.getGraphics();
+        g.setColor(0xffffff);
+        g.setAlpha(100);
+        g.setAntiAliased(true);
+        g.fillArc(0, 0, size, size, 0, 360);
+        Image selectedWalkthru = Image.createImage(size, size, 0);
+        g = selectedWalkthru.getGraphics();
+        g.setColor(0xffffff);
+        g.setAntiAliased(true);
+        g.fillArc(0, 0, size, size, 0, 360);
+        RadioButton[] rbs = new RadioButton[swipe.getTabCount()];
+        FlowLayout flow = new FlowLayout(CENTER);
+        flow.setValign(BOTTOM);
+        Container radioContainer = new Container(flow);
+        for(int iter = 0 ; iter < rbs.length ; iter++) {
+            rbs[iter] = RadioButton.createToggle(unselectedWalkthru, bg);
+            rbs[iter].setPressedIcon(selectedWalkthru);
+            rbs[iter].setUIID("Label");
+            radioContainer.add(rbs[iter]);
+        }
+                
+        rbs[0].setSelected(true);
+        swipe.addSelectionListener((i, ii) -> {
+            if(!rbs[ii].isSelected()) {
+                rbs[ii].setSelected(true);
+            }
+        });
+        
+        Component.setSameSize(radioContainer, spacer1, spacer2);
+        add(LayeredLayout.encloseIn(swipe, radioContainer));
+        
+          swipe.setUIID("Container");
+        swipe.getContentPane().setUIID("Container");
+        swipe.hideTabs();
+              setTitle("Add Artwork");
+        setLayout(BoxLayout.yCenter());
+        
+        System.out.println(ServiceActualite.getInstance().getAllOeuvres());
+                
+        TextField tftitreact = new TextField(""," titre ");
+        TextField tfdesc = new TextField("","Description ");
+//        TextField tfImageOeuvre = new TextField("","Oeuvre Image ");
+    //    TextField tfNomCat= new TextField("", "Oeuvre Category");
+        TextField tfetendu= new TextField("", " etendu");
+        TextField tfimg= new TextField("", " image");
+        
+
+//        TextField tfID_Artiste = new TextField("","Artist ID");
+        Button btnValider = new Button("Add actualite");
+     
+        btnValider.addActionListener(new ActionListener() {
+             @Override
+             public void actionPerformed(ActionEvent evt) {
+                 if ((tftitreact.getText().length()==0)||(tfdesc.getText().length()==0)||(tfetendu.getText().length()==0)||(tfimg.getText().length()==0))
+                    Dialog.show("Alert", "Please fill all the fields", new Command("OK"));
+                 else {
+                    try {
+                     
+                        
+                        if(ServiceActualite.getInstance().addAct(tftitreact, tfdesc,tfetendu ,tfimg))
+                            Dialog.show("Success","Connection accepted",new Command("OK"));
+                 
+                    } catch (NumberFormatException e) {
+                        Dialog.show("ERROR", "price must be a number", new Command("OK"));
+                    
+                   }
+  Dialog.show("Success","Connection accepted",new Command("OK"));
+                 }}
+        });
+      addAll(tftitreact,tfdesc,tfetendu,tfimg,btnValider);
+         getToolbar().addMaterialCommandToLeftBar("", FontImage.MATERIAL_ARROW_BACK, e-> previous.showBack());
+    
+    }
+    private void addTab(Tabs swipe, Label spacer) {
+        int size = Math.min(Display.getInstance().getDisplayWidth(), Display.getInstance().getDisplayHeight());
+     
+        Label overlay = new Label(" ", "ImageOverlay");
+        
+        Container page1 = 
+            LayeredLayout.encloseIn(
+                overlay,
+                BorderLayout.south(
+                    BoxLayout.encloseY(
+                           
+                            spacer
+                        )
+                )
+            );
+
+        swipe.addTab("", page1);
+    }
+}
